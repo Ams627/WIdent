05a5482ab1269830328943700a02a26331f3251e
Adrian Sims <adriansims62781@gmail.com>
2020-04-04 13:35:51 +0200

    Add project files.

---
+++ WIdent/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Principal;
+using System.DirectoryServices.AccountManagement;
+
+class Program
+{
+    private static void Main()
+    {
+        var user = WindowsIdentity.GetCurrent().User.Value;
+        Console.WriteLine($"{user}");
+        var sid = UserPrincipal.Current.Sid.ToString();
+        Console.WriteLine($"{sid}");
+    }
+}
